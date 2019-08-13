19394798b496417feda050db84e560f15bfa0b77
Lucas Abbade <lucasabbade@hotmail.com>
2019-08-13 16:58:14 -0300

    basic tests added

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import re
 
 def Validate(cep):
-    return False
+    """Valida o cep. 'cep' deve ser string, retorno booleano."""
+    if (len(cep) != 6) or not (100000 < int(cep) < 999999):         # cep fora do padrão básico
+        raise Exception(f'CEP "{cep}" inválido.')
+
+    return True
--- test.py
+++ test.py
@@ -4,12 +4,22 @@ from main import Validate
 class TestCodes(unittest.TestCase):
 
     def test_right_codes(self):
-        self.assertTrue('523563')
-        self.assertTrue('112233')
+        self.assertTrue(Validate('523563'))
+        self.assertTrue(Validate('112233'))
 
     def test_false_codes(self):
-        self.assertFalse('121426')
-        self.assertFalse('552523')
+        self.assertFalse(Validate('121426'))
+        self.assertFalse(Validate('552523'))
+
+    def test_invalid_size(self):
+        self.assertRaises(Exception, Validate, '')
+        self.assertRaises(Exception, Validate, '123')
+        self.assertRaises(Exception, Validate, '12345')
+        self.assertRaises(Exception, Validate, '1234567')
+
+    def test_range(self):
+        self.assertRaises(Exception, Validate, '100000')
+        self.assertRaises(Exception, Validate, '999999')
 
 if __name__ == '__main__':
     unittest.main()
