216280f6fb9ef7adba93d988505b1e50c82d0dde
Lucas Abbade <lucasabbade@hotmail.com>
2019-08-13 20:22:12 -0300

    finished web interface

--- static/main.js
+++ static/main.js
@@ -1,1 +1,28 @@
-alert('amigo sto aqui');
+const validateRoute = 'http://localhost:5000/validate';
+
+$("#validate-btn").click(() => {
+    const cep = $('#cep').val();
+    console.log(`Checking cep ${cep}`);
+
+    $.ajax({
+        url: validateRoute + '?cep=' + cep,
+        type: 'GET',
+        beforeSend: () => {
+            $("#validate-btn").attr("disabled", true);
+        },
+        success: (res) => {
+            console.log(`Received results: ${JSON.stringify(res)}`);
+            var text = `Cep ${res.cep} é `;
+            text += res.result ? 'válido' : 'inválido';
+
+            $("#result").text(text);
+            $("#results-div").removeAttr('hidden');
+        },
+        error: () => {
+            alert(`Erro ao consultar cep "${cep}"`);
+        },
+        complete: () => {
+            $("#validate-btn").attr("disabled", false);
+        }
+    });
+});
