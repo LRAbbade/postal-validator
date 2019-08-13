a2bfabc9c0f8e9bb2bc65e0ee23d2d41045bb2c8
Lucas Abbade <lucasabbade@hotmail.com>
2019-08-13 17:38:02 -0300

    frontend quase la

--- backend.py
+++ backend.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from main import Validate
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
+
+@app.route('/validate')
+def validate_cep():
+    cep = str(request.args.get('cep'))
+    try:
+        res = Validate(cep)
+    except Exception as e:
+        res = str(e)
+
+    return jsonify({
+        'cep': cep,
+        'result': res
+    })
