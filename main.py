7714ac7a08979d297048912dafd0ee9b2fdadea6
Lucas Abbade <lucasabbade@hotmail.com>
2019-08-13 17:07:50 -0300

    working

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import re
 
+check_alternate_pairs = re.compile(r'(\d)\d\1')
+check_size = re.compile(r'^\d{6}$')
+
 def Validate(cep):
     """Valida o cep. 'cep' deve ser string, retorno booleano."""
-    if (len(cep) != 6) or not (100000 < int(cep) < 999999):         # cep fora do padrão básico
-        raise Exception(f'CEP "{cep}" inválido.')
+    if (len(check_size.findall(cep)) == 0) or not (100000 < int(cep) < 999999):
+        # cep fora do padrão básico
+        raise Exception(f'CEP "{cep}" fora do padrão.')
 
-    return True
+    r = check_alternate_pairs.findall(cep)
+    return len(r) == 0
